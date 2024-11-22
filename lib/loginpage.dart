a9e1875eeea956fd6b399786d7511a3980f8dd20
refafairuz <refamarhas@gmail.com>
2024-11-22 10:47:38 +0700

    APAKEK

---
+++ lib/loginpage.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color.fromARGB(255, 243, 58, 243),
+            Color.fromARGB(255, 90, 79, 240),
+          ],
+        )),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: _page(),
+        ));
+  }
+
+  Widget _page() {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _icon(),
+            const SizedBox(
+              height: 50,
+            ),
+            _inputField("username", usernameController),
+            const SizedBox(
+              height: 20,
+            ),
+            _inputField("password", passwordController, isPassword: true),
+            const SizedBox(
+              height: 50,
+            ),
+            _LoginBtn(),
+            const SizedBox(
+              height: 20,
+            ),
+            _extraText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _icon() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white, width: 2),
+          shape: BoxShape.circle),
+      child: const Icon(Icons.person, color: Colors.white, size: 120),
+    );
+  }
+
+  Widget _inputField(String hintext, TextEditingController contoller,
+      {isPassword = false}) {
+    var border = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(18),
+        borderSide: const BorderSide(color: Colors.white));
+
+    return TextField(
+      style: const TextStyle(color: Colors.white),
+      controller: contoller,
+      decoration: InputDecoration(
+        hintText: hintext,
+        hintStyle: const TextStyle(color: Colors.white),
+        enabledBorder: border,
+        focusedBorder: border,
+      ),
+      obscureText: isPassword,
+    );
+  }
+
+  Widget _LoginBtn() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Username : " + usernameController.text);
+        debugPrint("Password : " + passwordController.text);
+      },
+      child: const SizedBox(
+          width: double.infinity,
+          child: Text("Sign in",
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 20))),
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.blue,
+        backgroundColor: Colors.white,
+        shape: const StadiumBorder(),
+        padding: const EdgeInsets.symmetric(vertical: 16),
+      ),
+    );
+  }
+
+  Widget _extraText() {
+    return const Text("Can't access to your account?",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 16, color: Colors.white));
+  }
+}
